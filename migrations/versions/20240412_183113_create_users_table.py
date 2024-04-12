b2411942bf377d86a2ba3a24d3f09507278db5c3
TStutsman <teagandstutsman@gmail.com>
2024-04-12 18:34:32 -0400

    added os lines to migration

--- migrations/versions/20240412_183113_create_users_table.py
+++ migrations/versions/20240412_183113_create_users_table.py
@@ -8,6 +8,9 @@ Create Date: 2024-04-12 18:31:13.381613
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'f74a60f7d0f4'
