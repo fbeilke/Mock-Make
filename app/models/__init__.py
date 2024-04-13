c316e2da439fecfdbb2f5a36011c931e8f0fbd2f
TStutsman <teagandstutsman@gmail.com>
2024-04-12 20:58:23 -0400

    added models and migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,8 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .product import Product
+from .product_image import ProductImage
+from .order import Order
+from .product_order import ProductOrder
+from .review import Review
+from .user_wish import UserWish
