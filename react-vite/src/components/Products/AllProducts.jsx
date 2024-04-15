5d7270e59813d029d21dd6a9f10c9daa0fa66dc0
Finn Beilke <beilke693@gmail.com>
2024-04-15 12:43:38 -0500

    Refactored AllProducts and ProductsByCategory to use ProductsList component

--- react-vite/src/components/Products/AllProducts.jsx
+++ react-vite/src/components/Products/AllProducts.jsx
@@ -1,16 +1,19 @@
 import { useEffect } from "react"
 import { getAllProducts } from "../../redux/products"
 import { addCartItemThunk } from "../../redux/session"
+import { getAllUsersThunk } from "../../redux/users"
 import { useDispatch, useSelector } from "react-redux"
-import { NavLink } from 'react-router-dom';
+import ProductsList from './ProductsList';
 import "./AllProducts.css"
 
 export default function AllProducts() {
     const dispatch = useDispatch();
     const { products } = useSelector(state => state.products)
+    const { users } = useSelector(state => state.users)
 
     useEffect(() => {
         dispatch(getAllProducts())
+        dispatch(getAllUsersThunk())
     }, [dispatch])
 
     const addToCart = (product) => {
@@ -24,23 +27,7 @@ export default function AllProducts() {
     return (
         <div className="products-page">
             <h2>All products</h2>
-            <div className="products-list">
-                {!products ? null : Object.values(products).map(product => (
-                    <div key={product.id} className="each-product">
-                        <NavLink to={`/products/${product.id}`} className="each-product-link">
-                            <img className="each-product-image" src={`${Object.values(product.product_images).filter(productImage => productImage.preview)[0].url}`} alt={`${product.name}`} />
-                            <div className="each-product-info">
-                                <p>{product.name}</p>
-                                <span>TODO: add reviews stars</span>
-                                <span>·</span>
-                                <span>TODO: add vendor name</span>
-                                <h3>${product.price}</h3>
-                            </div>
-                        </NavLink>
-                        <button onClick={() => addToCart(product)}>Add to cart</button>
-                    </div>
-                ))}
-            </div>
+            <ProductsList products={products} users={users}/>
         </div>
     )
 }
